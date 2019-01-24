f5d49cb8aa500d9eae110b83989594338bcc9131
Jon Eyolfson <jon@eyl.io>
2019-01-24 13:14:35 -0500

    Include templates to package

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ import setuptools
 setuptools.setup(
     name = 'django-cpp-doc',
     version = '0.1.0',
+    include_package_data = True,
     packages = setuptools.find_packages(),
+    zip_safe = False,
 )
